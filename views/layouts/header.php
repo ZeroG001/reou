e516729a4c022e830b3838cf009e746546e7e8c8
Blayne Holland <blayneholland@outlook.com>
2016-03-02 17:17:26 -0500

    adding nav styles

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,24 +1,34 @@
 <?php 
 	# course helpers
 	# course class
-	
 ?>
 
 
 <nav class="main_nav">
 
 	<div class="main_nav--logo"> 
-		<img src="" alt="logo" />
-		<span class="logo--text"> Logo text </span>
+		<img src="http://placehold.it/100x50" alt="logo" />
 	</div>
 
-	<a class="main_nav--item" href="#"> Item 1 </a>
+
+	<div class="main_nav--menu-button">
+		<div class="menu-button--bar"> </div>
+		<div class="menu-button--bar"> </div>
+		<div class="menu-button--bar"> </div>
+	</div>
+
+	<div class="main_nav--nav"> 
+
+		<a class="main_nav--item" href="#"> Sign-In </a>
+		<a class="main_nav--item" href="#"> Sign-Up </a>
 
 
-	<a class="main_nav--item" href="#"> Item 2 </a>
-	<nav class="main_subnav"> 
-		<a class="main_subnav--item" href="#"> Sub item 1 </a>
-		<a class="main_subnav--item" href="#"> Sub item 2 </a>
-	</nav>
+		<a class="main_nav--item" href="#"> More </a>
+		<nav class="main_subnav"> 
+			<a class="main_subnav--item" href="#"> My Courses </a>
+			<a class="main_subnav--item" href="#"> Sign Out</a>
+		</nav>
+
+	</div>
 
 </nav>
