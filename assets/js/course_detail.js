51a7e0ee26b631dbacea02f7faec7eb63186a65f
Blayne <blayneholland@outlook.com>
2017-03-17 16:56:33 -0400

    added responsive table to schedule

--- assets/js/course_detail.js
+++ assets/js/course_detail.js
@@ -1,8 +1,26 @@
 ( function($) {
 
 	$('.course_schedule--show-calendar').click(function() {
-		$('.course_schedule--calendar').hide();
-		$(this).parent().parent().next('.course_schedule--calendar').slideToggle();
+		$schedule_calendar = $(this).parent().parent().next('.course_schedule--calendar');
+		
+		// $schedule_calendar.toggle();
+		// If the calendar is showing
+		if($schedule_calendar.css('display') == "table-row") {
+
+			// Hide the calendar
+			$('.course_schedule--calendar').hide();
+			$schedule_calendar.css('display', 'none');
+
+		} else {
+
+			// Show the calendar
+			$('.course_schedule--calendar').hide();
+			$schedule_calendar.css('display', 'table-row');
+			setTimeout(function(){
+				alert('this is a test');
+			}, 1000);
+		}
+		
 	});
 
 }) ( jQuery );
