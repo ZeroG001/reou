58ea6e6c6d7114b3ff617bf7fde1a10046f78904
Blayne <blayneholland@outlook.com>
2017-03-20 17:17:00 -0400

    added animation to course schedule section

--- assets/js/course_detail.js
+++ assets/js/course_detail.js
@@ -1,25 +1,40 @@
 ( function($) {
 
 	$('.course_schedule--show-calendar').click(function() {
+
+// --------------- Animation for schedulede details button --------------- //
+
+	if ( $(this).hasClass('show-calendar_selected') ) {
+		$(this).removeClass('show-calendar_selected');
+		$('.course_schedule--show-calendar').removeClass('show-calendar_selected');
+
+	} 
+	else {
+		$('.course_schedule--show-calendar').removeClass('show-calendar_selected');
+		$(this).toggleClass('show-calendar_selected');
+	}
+	
+
+	
+
+// --------------- Animation for schedulede details button --------------- //
+
 		$schedule_calendar = $(this).parent().parent().next('.course_schedule--calendar');
-		
-		// $schedule_calendar.toggle();
+	
 		// If the calendar is showing
-		if($schedule_calendar.css('display') == "table-row") {
+		if ( $schedule_calendar.css('display') == "block" ) {
 
 			// Hide the calendar
-			$('.course_schedule--calendar').hide();
-			$schedule_calendar.css('display', 'none');
+			$('.course_schedule--calendar').slideUp(200);
+			$schedule_calendar.slideUp(200);
 
-		} else {
-
+		} 
+		else {
 			// Show the calendar
-			$('.course_schedule--calendar').hide();
-			$schedule_calendar.css('display', 'table-row');
-			setTimeout(function(){
-				alert('this is a test');
-			}, 1000);
+			$('.course_schedule--calendar').slideUp(200);
+			$schedule_calendar.slideDown(200);
 		}
+		// $schedule_calendar.slideToggle();
 		
 	});
 
