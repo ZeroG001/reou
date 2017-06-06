570e413905085c40334197138778048aac53f546
Blayne <blayneholland@outlook.com>
2017-06-06 17:17:33 -0400

    ok so the class creation page is going okay

---
+++ views/layouts/admin_sidebar.php
@@ -0,0 +1,16 @@
+	<div class="profile__sidebar-container">
+
+		<div class="profile__sidebar">
+
+			<div class="profile__logo">
+				<div class="profile__logo-image"> </div>
+				<a href=""> Change Photo </a>
+			</div>
+
+			<a class="profile__sidebar-item" href="<?php echo admin_route('show-users') ?>"> Users </a>
+			<a class="profile__sidebar-item" href="<?php ?>"> Courses </a>
+			<a class="profile__sidebar-item" href="<?php ?>"> Payments </a>
+
+		</div>
+
+	</div>
