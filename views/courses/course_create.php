0321e5b3b3c313db13a66a07d8139518d8a7f6c2
Blayne <blayneholland@outlook.com>
2017-06-07 17:09:32 -0400

    worked out styleing issues with course create page

--- views/courses/course_create.php
+++ views/courses/course_create.php
@@ -16,62 +16,66 @@
 ?>
 
 
-<!-- Courses. Thisgs we would liek to update -->
+	<!-- Courses. Thisgs we would liek to update -->
 
-<!-- 
+	<!-- 
 
-	- The Goal today is to be able to create a course then create a schedule for that course
-	- You should also be able to create and submit multiple schedules for the corse
-	- Multiple forms will be submitted using ajax
-
-
-
-	General Course Information
-	
-	- course_name
-	- course_desc
-	- category_id
-	- course_number
-	- course_cost (this might get complicated fast)
-	- course_location
-	- course_credits
-	- course_notes
-	- instructor_id
-	- min_class_size
-	- max_class_size (change from)
+		- The Goal today is to be able to create a course then create a schedule for that course
+		- You should also be able to create and submit multiple schedules for the corse
+		- Multiple forms will be submitted using ajax
 
 
 
-	Timing and Scheduling
+		General Course Information
+		
+		- course_name
+		- course_desc
+		- category_id
+		- course_number
+		- course_cost (this might get complicated fast)
+		- course_location
+		- course_credits
+		- course_notes
+		- instructor_id
+		- min_class_size
+		- max_class_size (change from)
 
-	- course_hours ( defines what time the course startes and begins. This might need to be defined separately )
-	- course_duration
+
+
+		Timing and Scheduling
+
+		- course_hours ( defines what time the course startes and begins. This might need to be defined separately )
+		- course_duration
 
 
 
-	Other
+		Other
 
-	- course_id
-	- active
+		- course_id
+		- active
 
 
--->
+	-->
 
 	<head>
 		<link rel="stylesheet" type="text/css" href="<?php echo asset_route('css') ?>main.css">
 	</head>
 
 
+
+	<!-- Sidebar -->
+	<?php require( D_ROOT . layout_route('admin-sidebar') ) ?>
+
+
 	<div class="profile__form-container">
 
-		<!-- Sidebar -->
-		<?php require( D_ROOT . layout_route('admin-sidebar') ) ?>
 
 		<?php display_alert('error'); ?>
 		<?php display_alert('alert'); ?>
 
 
 		<!-- Action should be the course acc place -->
+		<div class="profile__form-container">
 		<form method="POST" action="">
 
 			<h1> Loreum ipsum title </h1>
@@ -80,20 +84,22 @@
 
 
 			<!-- Items Related to course details -->
-			<fieldset>
-
 
 				<h3> Loreum Ipsum Info </h3>
 
 				<!-- Course Name -->
-				<label for="courseName"> Course Name </label>
-				<input type="text" name="courseName" id="courseName" value="<?php if( isset( $_POST['courseName']) ) { echo $_POST['courseName']; } ?>"> </input>
+				<div class="reou-form__input-group">
+					<label for="courseName"> Course Name </label>
+					<input type="text" name="courseName" id="courseName" value="<?php if( isset( $_POST['courseName']) ) { echo $_POST['courseName']; } ?>"> </input>
+				</div>
 
 
 				<!-- Course Description -->
-				<label for="courseDesc"> Course Descirption </label>
-				<input type="text" name="courseDesc" id="courseDesc" value="<?php if( isset( $_POST['courseName']) ) { echo $_POST['courseDesc']; } ?>">
-				
+				<div class="reou-form__input-group">
+					<label for="courseDesc"> Course Descirption </label>
+					<input type="text" name="courseDesc" id="courseDesc" value="<?php if( isset( $_POST['courseName']) ) { echo $_POST['courseDesc']; } ?>">
+				</div>
+
 
 				<!-- This information will have to pull in from the course category query -->
 				<label for="categoryId"> Category </label>
@@ -110,17 +116,20 @@
 					<input type="text" class="reou-form__input" name="courseNumber" id="courseNumber" value="<?php if( isset( $_POST['courseName']) ) { echo $_POST['courseNumber']; } ?>">
 				</div>
 
+
 				<!-- Course Cost (Number Only) -->
 				<div class="reou-form__input-group">
 					<label for="courseCost"> Course Cost </label>
 					<input type="text" class="reou-form__input" name="courseCost" id="courseCost" value="<?php if( isset( $_POST['courseCost']) ) { echo $_POST['courseCost']; } ?>">
 				</div>
 
+
 				<!-- Course Location -->
 				<div class="reou-form__input-group">
 					<label for="courseLocation"> Course Location </label>
 					<input type="text" class="reou-form__input" name="courseLocation" id="courseLocation" value="<?php if( isset( $_POST['courseLocation']) ) { echo $_POST['courseLocation']; } ?>">
 				</div>
+
 
 				<!-- Course Credits (Number Only) -->
 				<div class="reou-form__input-group">
@@ -139,24 +148,27 @@
 				<!-- Instructor ID-->
 				<div class="reou-form__input-group">
 					<label for="instructorId"> Instructor </label>
-					<input type="text" name="instructorId" value="<?php if( isset( $_POST['instructorId']) ) { echo $_POST['instructorId']; } ?>">
+					<input type="text" class="reou-form__input" name="instructorId" value="<?php if( isset( $_POST['instructorId']) ) { echo $_POST['instructorId']; } ?>">
 				</div>
 
 
 				<!-- Min Class Size -->
 				<div class="reou-form__input-group">
-				<label for="minClassSize"> Min Class Size </label>
-				<input type="text" name="minClassSize" id="minClassSize" value="<?php if( isset( $_POST['minClassSize']) ) { echo $_POST['minClassSize']; } ?>"> 
+					<label for="minClassSize"> Min Class Size </label>
+					<input type="text" class="reou-form__input" name="minClassSize" id="minClassSize" value="<?php if( isset( $_POST['minClassSize']) ) { echo $_POST['minClassSize']; } ?>"> 
 				</div>
 
 
 				<!-- Max Class Size -->
-				<label for="maxClassSize"> Max Class Size </label>
-				<input type="text" name="maxClassSize" id="maxClassSize" value="<?php if( isset( $_POST['maxClassSize']) ) { echo $_POST['maxClassSize']; } ?>">
+				<div class="reou-form__input-group">
+					<label for="maxClassSize"> Max Class Size </label>
+					<input type="text" class="reou-form__input" name="maxClassSize" id="maxClassSize" value="<?php if( isset( $_POST['maxClassSize']) ) { echo $_POST['maxClassSize']; } ?>">
+				</div>
 
 
 				<!-- Course Active? -->
 				<label for="active"> Course Active </label>
+
 				<span> Yes </span>
 				<input type="radio" name="active" value="1" <?php if( isset( $_POST['active'] ) && $_POST['active'] == '1' ) { echo "checked"; } elseif ( !isset( $_POST['active'] ) ) { echo "checked"; } ?>>
 
@@ -166,9 +178,8 @@
 				<input type="hidden" name="_method" value="post">
 				<input type="submit" value="Create Page">
 
-			</fieldset>
-
 		</form>
+	</div>
 
 	</div>
 
